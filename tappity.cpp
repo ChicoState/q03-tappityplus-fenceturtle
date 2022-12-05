8ec1193575f35e07e34a00225ef6d42f4d33dc32
diana tyler <rtyler@mail.csuchico.edu>
2022-12-05 00:35:37 -0800

    Implemented class functions and unit tests

--- tappity.cpp
+++ tappity.cpp
@@ -1,17 +1,23 @@
 #include "tappity.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
 /**
  * Class for tappity
 **/
 
 //Constructor sets the reference phrase
-tappity::tappity(std::string reference)
+tappity::tappity(std::string reference) : reference(reference)
 {
+  userinput = "";
 }
 
-//Provide the input to be compared to the reference. Before this 
+//Provide the input to be compared to the reference. Before this
 //function is called, the input should be considered an empty string
 void tappity::entry(std::string input)
 {
+  userinput = input;
 }
 
 //Compares the length of the reference to that of the input and
@@ -19,16 +25,16 @@ void tappity::entry(std::string input)
 //same length
 int tappity::length_difference()
 {
-  return 0;
+  return abs(userinput.length() - reference.length());
 }
 
 //Compares the content of the reference to that of the input and
 //calculates the accuracy of matching characters. If the two strings
-//are identical, accuracy should be 100. However, for each 
+//are identical, accuracy should be 100. However, for each
 //corresponding character that does not match in the same location,
 //the percentage of corresponding characters should be returned.
 //For example, if the reference and input have 10 letters and the input
-//matches 8 of the corresponding reference characters, the function 
+//matches 8 of the corresponding reference characters, the function
 //should return 80. When input does not have the same number of
 //characters as the reference, the accuracy should represent the percent
 //of matching characters between the shorter and longer strings. For
@@ -36,5 +42,24 @@ int tappity::length_difference()
 //locations in another string that has 16 characters, the accuracy is 50.
 double tappity::accuracy()
 {
-  return 0;
+  int matching = 0;
+  std::string shorter = userinput;
+  std::string longer = reference;
+  if(shorter.length() > longer.length()){
+    shorter = reference;
+    longer = userinput;
+  }
+
+  for(int i = 0; i < shorter.length(); i++){
+    if(shorter[i] == longer[i]){
+      matching++;
+    }
+  }
+  if(longer.length() == 0){
+    return 100;
+  }
+
+  double acc = ((double)matching/longer.length()) * 100;
+
+  return (int)acc;
 }
--- tappityTest.cpp
+++ tappityTest.cpp
@@ -4,12 +4,99 @@
 
 #include <gtest/gtest.h>
 #include "tappity.h"
- 
+
 class tappityTest : public ::testing::Test
 {
 	protected:
-		tappityTest(){} 
-		virtual ~tappityTest(){} 
+		tappityTest(){}
+		virtual ~tappityTest(){}
 		virtual void SetUp(){}
 		virtual void TearDown(){}
 };
+
+TEST(tappityTest, samestring){
+  std::string test = "abcdef";
+  tappity mytap(test);
+  mytap.entry(test);
+  ASSERT_EQ(mytap.accuracy(), 100);
+}
+
+TEST(tappityTest, inputshorter){
+  std::string input = "abcd";
+  std::string ref = "abcdefgh";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.accuracy(), 50);
+}
+
+TEST(tappityTest, refshorter){
+  std::string input = "abcd";
+  std::string ref = "abcdefgh";
+  tappity mytap(input);
+  mytap.entry(ref);
+  ASSERT_EQ(mytap.accuracy(), 50);
+}
+
+TEST(tappityTest, inputempty){
+  std::string input = "";
+  std::string ref = "abcdefgh";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.accuracy(), 0);
+}
+
+TEST(tappityTest, refempty){
+  std::string input = "abc";
+  std::string ref = "";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.accuracy(), 0);
+}
+
+TEST(tappityTest, bothepmty){
+  std::string input = "";
+  std::string ref = "";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.accuracy(), 100);
+}
+
+TEST(tappityTest, uneven){
+  std::string input = "abcd";
+  std::string ref = "abcdefghijk";
+  tappity mytap(input);
+  mytap.entry(ref);
+  ASSERT_EQ(mytap.accuracy(), 36);
+}
+
+TEST(tappityTest, uninit){
+  std::string input;
+  std::string ref = "abc";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.accuracy(), 0);
+}
+
+TEST(tappityTest, bigstring){
+  std::string input = "T";
+  std::string ref = "The quick brown fox jumped over the lazy dog. esafjklafhdskljfhdasjk";
+  tappity mytap(input);
+  mytap.entry(ref);
+  ASSERT_EQ(mytap.accuracy(), 1);
+}
+
+TEST(tappityTest, lengthdiff){
+  std::string input = "a";
+  std::string ref = "abc";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.length_difference(), 2);
+}
+
+TEST(tappityTest, lengthdiffempty){
+  std::string input = "";
+  std::string ref = "";
+  tappity mytap(ref);
+  mytap.entry(input);
+  ASSERT_EQ(mytap.length_difference(), 0);
+}
